3d734aa9c88531cdaa4f04109bfac3d299b8aefe
YukiMariNishi <mnishioca@gmail.com>
2025-08-13 13:44:42 -0300

    sheets

--- sheets.js
+++ sheets.js
@@ -1,31 +1,14 @@
 const { google } = require('googleapis');
-const path = require('path');
 
 const SPREADSHEET_ID = '1-u-WSLEpz7_537FytU_gK3bm3sKm-8Rkw2Fn3MFvRJ4';
+const CREDENTIALS_PATH = process.env.GOOGLE_CREDENTIALS_BASE64 || 'credentials.json';
 
 async function autenticar() {
-  // Se a variável de ambiente GOOGLE_CREDENTIALS_BASE64 existir, use-a
-  if (process.env.GOOGLE_CREDENTIALS_BASE64) {
-    console.log('Usando credenciais da variável de ambiente (Base64).');
-    
-    // Decodifica a string Base64 para JSON
-    const credentials = JSON.parse(Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64, 'base64').toString('utf-8'));
-    
-    const auth = new google.auth.GoogleAuth({
-      credentials: credentials,
-      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-    });
-
-    return await auth.getClient();
-  } else {
-    // Caso contrário, use o arquivo local para desenvolvimento
-    console.log('Usando arquivo de credenciais local para desenvolvimento.');
-    const auth = new google.auth.GoogleAuth({
-      keyFile: path.join(__dirname, 'credentials.json'),
-      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-    });
-    return await auth.getClient();
-  }
+  const auth = new google.auth.GoogleAuth({
+    keyFile: CREDENTIALS_PATH,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
+  return await auth.getClient();
 }
 
 async function adicionarNoticias(noticias, sheetName) {
