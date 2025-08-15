779fd2f544aa4c24c45e615cdd558d8b84a8274a
YukiMariNishi <mnishioca@gmail.com>
2025-08-15 10:24:23 -0300

    AAAAAAAAAAAAAAAAAA

--- sheets.js
+++ sheets.js
@@ -6,23 +6,23 @@ const fs = require('fs');
 const SPREADSHEET_ID = '1-u-WSLEpz7_537FytU_gK3bm3sKm-8Rkw2Fn3MFvRJ4';
 
 // --- CRIAÇÃO TEMPORÁRIA DO ARQUIVO DE CREDENCIAIS ---
-const CREDENTIALS_PATH = path.join(__dirname, 'credentials-temp.json');
+const PUPPETEER_EXECUTABLE_PATH = path.join(__dirname, 'credentials-temp.json');
 
-if (!fs.existsSync(CREDENTIALS_PATH)) {
-  if (!process.env.GOOGLE_CREDENTIALS) {
+if (!fs.existsSync(PUPPETEER_EXECUTABLE_PATH)) {
+  if (!process.env.PUPPETEER_EXECUTABLE_PATH) {
     console.error('[ERRO FATAL] A variável de ambiente GOOGLE_CREDENTIALS não foi definida!');
     process.exit(1);
   }
-  fs.writeFileSync(CREDENTIALS_PATH, process.env.GOOGLE_CREDENTIALS);
+  fs.writeFileSync(PUPPETEER_EXECUTABLE_PATH, process.env.PUPPETEER_EXECUTABLE_PATH);
   console.log('[INFO] Arquivo de credenciais temporário criado com sucesso.');
 }
 
 // --- DEPURAÇÃO ---
 console.log('--- INICIANDO VERIFICAÇÃO DE CREDENCIAIS ---');
 console.log(`[INFO] O script 'sheets.js' está na pasta: ${__dirname}`);
-console.log(`[INFO] Procurando pelo arquivo de credenciais em: ${CREDENTIALS_PATH}`);
+console.log(`[INFO] Procurando pelo arquivo de credenciais em: ${PUPPETEER_EXECUTABLE_PATH}`);
 
-if (fs.existsSync(CREDENTIALS_PATH)) {
+if (fs.existsSync(PUPPETEER_EXECUTABLE_PATH)) {
     console.log('[SUCESSO] O arquivo de credenciais TEMPORÁRIO foi ENCONTRADO.');
 } else {
     console.error('[ERRO FATAL] O arquivo de credenciais TEMPORÁRIO NÃO FOI ENCONTRADO!');
@@ -32,7 +32,7 @@ console.log('--- FIM DA VERIFICAÇÃO ---');
 // --- FUNÇÕES ---
 async function autenticar() {
   const auth = new google.auth.GoogleAuth({
-    keyFile: CREDENTIALS_PATH,
+    keyFile: PUPPETEER_EXECUTABLE_PATH,
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
   });
   return await auth.getClient();
